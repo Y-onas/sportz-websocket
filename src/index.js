ded72df1939e8e49329afa317fcf4ef41f888762
Y-onas <164673912+Y-onas@users.noreply.github.com>
2026-02-11 17:40:28 +0300

    implement matches POST and GET routes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { matchesRouter } from "./routes/matches.js";
 const app = express();
 const PORT = 8000;
 app.use(express.json());
@@ -6,6 +7,8 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
+
+app.use("/matches", matchesRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
